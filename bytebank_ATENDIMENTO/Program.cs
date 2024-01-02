c57435fa917a376f26b1c9c90d559f240654318d
DieleCorrea <dybs1520@gmail.com>
2024-01-02 11:23:27 -0300

    Calculando a média em um array

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,4 +1,26 @@
 ﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
 Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
-new ByteBankAtendimento().AtendimentoCliente();
+//new ByteBankAtendimento().AtendimentoCliente();
 
+//chamar o metodo
+TestaArrayInt();
+
+void TestaArrayInt() {
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+
+    Console.WriteLine($"Tamanho do Array{idades.Length}");
+    int acumulador = 0;
+    for (int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades[i];
+        Console.WriteLine($"indice [{i}] = {idade}");
+        acumulador += idade;
+    }
+     int media = acumulador / idades.Length;
+    Console.WriteLine($"Media de idades = {media}");
+}
