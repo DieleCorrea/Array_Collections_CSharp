84eeee2f8d33e4e5bb2d3cb41c96d13b8342ef7c
DieleCorrea <dybs1520@gmail.com>
2024-01-04 09:00:31 -0300

    metodo novo

---
+++ bytebank_ATENDIMENTO/Exercicios/ArrayComObjetos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ATENDIMENTO.Exercicios
+{
+    internal class ArrayComObjetos
+    {
+        private string[] obj = null;
+
+        public ArrayComObjetos(string[] item)
+        {
+            this.obj = item;
+        }
+    }
+}
